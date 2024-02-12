a11764328336dd67210704928d22c77961f98111
NutchanonRungthong <anonimoseza@gmail.com>
2024-02-12 10:01:39 +0700

    update2

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended:false}))
 
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public/views'))
 
 app.get('/', async(req,res)=>{
    try{
